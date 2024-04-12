e5e154f9c2192273eefa62e189103f5f1e0c012e
MaxCodeCraft <maxcodecraft06@gmail.com>
2024-04-12 10:34:36 +0200

    *

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 fetch("https://whatyearisit-backend-lilac-eta.vercel.app/year/")
   .then((r) => r.json())
   .then((data) => {
-    console.log(data);
     document.querySelector("#year").textContent = data.year;
   });
