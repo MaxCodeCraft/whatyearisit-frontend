d7fd8f42ac90bc85dae75a22dc79a3dc665b71e7
MaxCodeCraft <maxcodecraft06@gmail.com>
2024-04-12 10:27:08 +0200

    updated domain

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-function getYear() {
-  fetch("https://whatyearisit-backend-lilac-eta.vercel.app/year/")
-    .then((r) => r.json())
-    .then((data) => {
-      console.log(data);
-      document.querySelector("#year").textContent = data.year;
-    });
-}
-
-getYear();
+fetch("https://whatyearisit-backend-lilac-eta.vercel.app/year/")
+  .then((r) => r.json())
+  .then((data) => {
+    console.log(data);
+    document.querySelector("#year").textContent = data.year;
+  });
